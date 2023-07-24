92a0395e863926baf6af13dab4a01b861f684a74
D-Wolter <wolter.daniel@gmail.com>
2023-07-24 10:32:20 -0300

    feat: criados testes para CollectionTypeServiceTest e CollectionTypeControllerTest

--- src/test/java/com/betrybe/museumfinder/solution/CollectionTypeControllerTest.java
+++ src/test/java/com/betrybe/museumfinder/solution/CollectionTypeControllerTest.java
@@ -1,5 +1,38 @@
 package com.betrybe.museumfinder.solution;
 
-public class CollectionTypeControllerTest {
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+@SpringBootTest
+@AutoConfigureMockMvc
+
+class CollectionTypeControllerTest {
+
+  @Autowired
+  private MockMvc mockMvn;
+
+  @Test
+  void testSucessCaseGetCollectioCount() throws Exception {
+    mockMvn.perform(MockMvcRequestBuilders.get("/collections/count/arte"))
+        .andExpect(MockMvcResultMatchers.status().isOk());
+  }
+
+  @Test
+  void testSucessCaseCheckPathParams() throws Exception {
+    mockMvn.perform(MockMvcRequestBuilders.get("/collections/count/arte,tecnologia"))
+        .andExpect(MockMvcResultMatchers.status().isOk());
+  }
+
+  @Test
+  void testSucessCaseGetAllCollectioCount() throws Exception {
+    mockMvn.perform(MockMvcRequestBuilders.get("/collections/count/arte,tecnologia"))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.collectionTypes").isArray())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.count").value("172"));
+  }
 }
--- src/test/java/com/betrybe/museumfinder/solution/CollectionTypeServiceTest.java
+++ src/test/java/com/betrybe/museumfinder/solution/CollectionTypeServiceTest.java
@@ -1,5 +1,48 @@
 package com.betrybe.museumfinder.solution;
 
-public class CollectionTypeServiceTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
+import com.betrybe.museumfinder.database.MuseumFakeDatabase;
+import com.betrybe.museumfinder.dto.CollectionTypeCount;
+import com.betrybe.museumfinder.service.CollectionTypeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+class CollectionTypeServiceTest {
+
+  @Mock
+  private MuseumFakeDatabase museumFakeDatabase;
+
+  @InjectMocks
+  private CollectionTypeService collectionTypeService;
+
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @Test
+  void testSucessCaseGetCollectioCount() {
+    String collectionType = "arte";
+    when(museumFakeDatabase.countByCollectionType(collectionType)).thenReturn(69L);
+
+    CollectionTypeCount result = collectionTypeService.countByCollectionTypes(collectionType);
+    assertEquals(1, result.collectionTypes().length);
+    assertEquals(collectionType, result.collectionTypes()[0]);
+    assertEquals(69L, result.count());
+  }
+
+  @Test
+  void testSucessCaseGetAllCollectioCount() {
+    String[] collectionTypes = {"arte", "tecnologia"};
+    when(museumFakeDatabase.countByCollectionType("arte")).thenReturn(69L);
+    when(museumFakeDatabase.countByCollectionType("tecnologia")).thenReturn(99L);
+
+    CollectionTypeCount result = collectionTypeService.countByCollectionTypes("arte,tecnologia");
+    assertEquals(collectionTypes.length, result.collectionTypes().length);
+  }
 }
